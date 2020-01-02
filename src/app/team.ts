621c93267c2abd800e339ba79a80892df0fcee43
Marcos Fdez <marcos>
2020-01-02 11:51:29 +0100

    Final version

--- src/app/team.ts
+++ src/app/team.ts
@@ -1,9 +1,9 @@
 export const Team = [
-    { id: 1, name: 'Arsenal',    image: 'assets/logos/arsenal.jpg', location: 'London (Holloway)', stadium:    'Emirates Stadium',
+    { id: 1, name: 'Arsenal',    image: 'assets/logos/arsenal.png', location: 'London (Holloway)', stadium:    'Emirates Stadium',
     capacity:    60704, manager: 'Unai Emery', captain: 'Pierre-Emerick Aubameyang', lat: 51.554929, lng: -0.108449 },
-    { id: 2, name: 'Aston Villa',    image: 'assets/logos/aston-villa.jpg', location:    'Birmingham', stadium: 'Villa Park',
+    { id: 2, name: 'Aston Villa',    image: 'assets/logos/aston-villa.png', location:    'Birmingham', stadium: 'Villa Park',
     capacity: 42785, manager: 'Dean Smith', captain: 'Jack Grealish', lat: 52.509131, lng: -1.884767 },
-    { id: 3, name: 'Bournemouth',    image: 'assets/logos/bornemouth.jpg',    location:    'Bournemouth', stadium:    'Dean Court',
+    { id: 3, name: 'Bournemouth',    image: 'assets/logos/bornemouth.png',    location:    'Bournemouth', stadium:    'Dean Court',
     capacity: 11364, manager: 'Eddie Howe', captain: 'Simon Francis', lat: 50.735278, lng: -1.838290 },
     { id: 4, name: 'Brighton & Hove Albion',    image: 'assets/logos/brighton.png', location: 'Brighton', stadium:    'Falmer Stadium',
     capacity: 30666, manager: 'Graham Potter', captain: 'Lewis Dunk', lat: 50.861606, lng: -0.083716 },
